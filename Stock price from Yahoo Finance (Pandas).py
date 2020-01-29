97e68a59c2e797b65acf83357a903d982e13e7d5
Carole Ouedraogo <56394514+Carole-Ouedraogo@users.noreply.github.com>
2020-01-29 13:24:17 -0500

    Update Stock price from Yahoo Finance (Pandas).py

--- Stock price from Yahoo Finance (Pandas).py
+++ Stock price from Yahoo Finance (Pandas).py
@@ -1,41 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[79]:
-
-
 print('Tesla Stock Price Trends In 2019')
-
-
-# In[80]:
-
 
 from pandas_datareader import data
 
-
-# In[81]:
-
+#2019 stock trends of Tesla Inc as a DataFrame.
 
 tesla = data.DataReader('tsla', start='2019-01-01', end='2019-12-31', data_source='yahoo')
 tesla.head()
 
-
-# In[82]:
-
-
+# How to display a single column
 print(tesla[['Close']])
-
-
-# In[71]:
-
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
 import seaborn; seaborn.set()
 
-
-# In[72]:
-
-
+# Visualizing with the plot() method
 tesla.plot()
+pandas-datareader
 
